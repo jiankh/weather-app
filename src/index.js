2e1cedbecda54d27c7f598d832c30190bb5de04e
jiankh <131821436+jiankh@users.noreply.github.com>
2023-10-04 18:52:45 -0400

    added favicon and logo picture

--- src/index.js
+++ src/index.js
@@ -12,10 +12,12 @@ searchform.addEventListener('submit', async (e) => {
     e.preventDefault()
     const searchInput = document.querySelector("#search").value
     url = `http://api.weatherapi.com/v1/forecast.json?key=a78d8065ac934df5b78171003230310&q=${searchInput}&days=3&aqi=no&alerts=no`
-    await loadCurrentWeather(url);
+    await loadCurrentWeather(url)
 
     clearHourlyContainer()
-    await loadHourlyWeather(url);
+    await loadHourlyWeather(url)
+
+    searchInput.clear()
 })
 
 
